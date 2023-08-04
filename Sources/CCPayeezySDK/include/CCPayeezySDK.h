53e6ccb4c908174aa3a404996817a5b93ad447f0
muhammadahtazaz <86792592+muhammadahtazaz@users.noreply.github.com>
2023-08-04 20:23:35 +0500

    initial Commit for CCPayeezySDK migration from Carthage to SPM
    Signed-off-by: muhammadahtazaz <muhammadahtazaz@carecloud.com>

---
+++ Sources/CCPayeezySDK/include/CCPayeezySDK.h
@@ -0,0 +1,19 @@
+//
+//  CCPayeezySDK.h
+//  CCPayeezySDK
+//
+//  Created by Daniyal Ayoob on 30/01/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CCPayeezySDK.
+FOUNDATION_EXPORT double CCPayeezySDKVersionNumber;
+
+//! Project version string for CCPayeezySDK.
+FOUNDATION_EXPORT const unsigned char CCPayeezySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CCPayeezySDK/PublicHeader.h>
+
+
+#import <PayeezySDK.h>
---
+++ Tests/CCPayeezySDKTests/CCPayeezySDKTests.m
@@ -0,0 +1,36 @@
+//
+//  CCPayeezySDKTests.m
+//  CCPayeezySDKTests
+//
+//  Created by Daniyal Ayoob on 30/01/2023.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CCPayeezySDKTests : XCTestCase
+
+@end
+
+@implementation CCPayeezySDKTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
